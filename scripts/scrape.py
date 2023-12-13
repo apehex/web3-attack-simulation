287dd98be0fe92ea0804090ea53fc6c7827680ac
apehex <apehex@protonmail.com>
2023-12-14 00:12:25 +0100

    Scrape only unknown tx

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import pprint
@@ -5,17 +6,77 @@ import web3
 
 import forta_toolkit.parsing.common as fpc
 
+# ARGS ########################################################################
+
+def main():
+    # CLI args
+    __parser = argparse.ArgumentParser(description='scrapes tx data from the RPC URL')
+    __parser.add_argument('--output', '-o', help='', default='samples/{case}/transactions/')
+    __parser.add_argument('--url', '-u', help='', default=os.environ.get('RPC_URL', 'https://rpc.ankr.com/eth'))
+    __parser.add_argument('--txhash', '-t', help='', default='')
+    __parser.add_argument('--list', '-l', help='', default='scripts/eth.txt')
+    # setup
+    __args = __parser.parse_args()
+    __provider = web3.Web3(web3.HTTPProvider(__args.url))
+    # collect a single TX
+    if __args.txhash:
+        __tx = scrape(provider=__provider, txhash=__args.txhash)
+        export_tx(tx=__tx, path='.')
+    # collect a list of TXs
+    if __args.list:
+        __attacks = import_tx_list(path=__args.list)
+        for __protocol, __txs in __attacks.items():
+            # parent path
+            __path = __args.output.format(case=__protocol)
+            os.makedirs(name=__path, exist_ok=True)
+            # iterate over TXs
+            for __h in __txs:
+                # check whether the file exists
+                if not (__h in ''.join(os.listdir(__path))):
+                    __tx = scrape(provider=__provider, txhash=__h)
+                    export_tx(tx=__tx, path=__path)
+
+# WRANGLING ###################################################################
+
 def clean(tx: dict) -> dict:
+    """Convert RPC object to a std dict of HEX strings."""
     return {__k: fpc.to_hexstr(__v, prefix=True) for __k, __v in tx.items()}
 
-__h = '0xe0a98402cb9b9536b4b308458ba46f23d41a51dfd6ee4102c118230a44529cbd'
-__u = os.environ.get('RPC_URL', 'https://rpc.ankr.com/eth')
-__p = web3.Web3(web3.HTTPProvider(__u))
+# COLLECT #####################################################################
 
-__tx = __p.eth.get_transaction(__h)
-__path = '{block}.{tx}.json'.format(block=__tx.get('blockNumber', 0), tx=__h)
+def scrape(provider: web3.Web3, txhash: str) -> dict:
+    """Collect and format TX data from a RPC endpoint."""
+    __tx = provider.eth.get_transaction(txhash)
+    return clean(__tx)
 
-with open(__path, 'w') as __file:
-    json.dump(obj=clean(__tx), fp=__file, indent=4)
+# __h in ''.join(os.listdir())
+# if not os.path.exists(__filename):
 
-# os.path.exists(__path)
+# IO ##########################################################################
+
+def import_tx_list(path: str) -> dict:
+    """Import the list of TXs for each protocol attack."""
+    __attacks  = {}
+    if os.path.exists(path):
+        with open(path, 'r') as __file:
+            __lines = __file.readlines()
+            for __l in __lines:
+                __parts = __l.split(' ')
+                __txs = __parts[0].strip().split(',')
+                __key = __parts[-1].strip()
+                __attacks[__key] = __txs
+    return __attacks
+
+def export_tx(tx: dict, path: str) -> None:
+    """Export a single TX data as a JSON file."""
+    __hash = tx.get('hash', 0)
+    __block = tx.get('blockNumber', 0)
+    __filename = '{block}.{tx}.json'.format(block=fpc.to_int(__block), tx=__hash)
+    __filepath = os.path.join(path, __filename)
+    with open(__filepath, 'w') as __file:
+        json.dump(obj=clean(tx), fp=__file, indent=4)
+
+# MAIN ########################################################################
+
+if __name__ == '__main__':
+    main()
