5f62dce654b55d39728b54c9f9669e84bd0816e8
apehex <apehex@protonmail.com>
2023-12-14 14:13:45 +0100

    Export the fuzzing wordlists

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -1,12 +1,18 @@
+# UNIQUE SYLLABLES ############################################################
+
 def syllable(index: int=0, size: int=2, offset: int=0xaaaa) -> str:
     __format = '{0:0>' + str(2 * size) + 'x}'
     return __format.format(offset + index)
+
+# GENERATOR ###################################################################
 
 def syllables(count: int=16, size: int=2, offset: int=0xaaaa) -> str:
     __index = 0
     while __index < count:
         yield syllable(index=__index, size=size, offset=offset)
         __index = __index + 1
+
+# WORDS #######################################################################
 
 def word(source: iter, left: int=0, right: int=0, prefix: bool=False) -> str:
     __count = 16 - left - right
@@ -16,11 +22,18 @@ def word(source: iter, left: int=0, right: int=0, prefix: bool=False) -> str:
         + ''.join([next(source) for __i in range(__count)])
         + right * '0000')
 
-# def pattern(chunks: int) -> str:
-#     return ''.join([ for __i in range(128)])
+# FUZZING WORDLISTS ###########################################################
 
-SYLLABLES = syllables(count=1024, size=2, offset=0xaaaa)
+SYLLABLES = syllables(count=2**16, size=2, offset=0xaaaa)
 
-# 4 non addresses
-ADDRESSES = [word(source=SYLLABLES, left=6, right=0, prefix=True) for _ in range(32)]
-NON_ADDRESSES = [word(source=SYLLABLES, left=0, right=0, prefix=True) for _ in range(32)]
+# IO ##########################################################################
+
+# address-like inputs
+with open('scripts/wordlists/inputs.address.txt', 'w') as __file:
+    __inputs =  [word(source=SYLLABLES, left=6, right=0, prefix=True) + '\n' for _ in range(512)]
+    __file.writelines(__inputs)
+
+# non-address
+with open('scripts/wordlists/inputs.bytes.large.txt', 'w') as __file:
+    __inputs =  [word(source=SYLLABLES, left=0, right=0, prefix=True) + '\n' for _ in range(512)]
+    __file.writelines(__inputs)
